0a0b2ed0c462db5047bf638a32846de5022fffd0
jolega <jolegacm@gmail.com>
2025-04-19 06:59:08 -0500

    validate data in html

---
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrls: ["./navbar.component.css"],
+})
+export class NavbarComponent {
+
+}
